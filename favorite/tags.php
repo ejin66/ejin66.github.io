79b8733e943f51e249d35a44f83e948867682e64
ejin66 <250022190@qq.com>
2018-06-12 08:41:59 +0800

    Delete tags.php

--- favorite/tags.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once('DB.php');
-	$db = new DB();
-	$sql = "select id,tagName from ft_tag where enable = 1 order by id asc";
-	$result = $db->query($sql);
-	//header("Access-Control-Allow-Origin:*");
-	echo json_encode($result);
-?>
